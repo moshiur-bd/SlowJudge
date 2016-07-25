0fe30fd5efd72cb6e6111557dfd7e389f9771651
moshiur-bd <moshiur.rabby@gmail.com>
2016-07-25 12:01:09 +0600

    judgeCDB running mechanism added
    now it can keep running a contest automatically

---
+++ run contest.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#include<windows.h>
+int t=0;
+int main(int argc,char *argv[]){
+    while(t++<atoi(argv[1])){
+        system("judgeCDB.bat");
+        Sleep(5000);
+    }
+    return 0;
+}
---
+++ run.bat
@@ -0,0 +1,1 @@
+"run contest.exe" 200000
