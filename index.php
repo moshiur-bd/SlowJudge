ed3e4077269b3b3f882dd038c4dee0da9af4c2b9
Md. Moshiur Rahman <moshiur.rabby@gmail.com>
2016-12-24 03:22:46 +0600

    wrote a intro

--- index.php
+++ index.php
@@ -3,7 +3,34 @@
 <?php
 include(__DIR__."\\header.php");
 ?>
-Welcome to slowjudge
+
+
+
+<div class='intro'>
+		<h2>Intro:</h2>
+		<p>Slowjudge is a Desktop Based Programming Contest platform.
+		 It's lightweight. A single Personal Computer can be used as host.
+		 This is the user-friendly replacement of widely used judging platform PC^2.
+		 It uses serial judging system as there is very little amount of resources in a desktop computer than a server.
+		 Serial judging is slow. that's why the platform is named slowjudge. Thouh it is fast enough for a small number of contestant around 50-100.
+		 </p>
+		 <h2>Current Features:</h2>
+		<ul>
+			<li>concurrent multiple contests</li>
+			<li>problem archive</li>
+			<li>multiple IO for a problem</li>
+			<li>atuomatic judging</li>
+			<li>source-code viewing</li>
+			<li>after contest submission</li>
+		</ul>
+		<h2>Applicable to:</h2>
+			<ul>
+				<li>host a programming contest</li>
+				<li>train programming</li>
+				<li>Lab work submission</li>
+				<li>Lab work assesment</li>
+			</ul>
+</div>
 
 </div>
 	<?php include("footer.php"); ?>
